222fd997758514b388cdabdef610042560e9bc9f
thanard <thanard.kurutach@berkeley.edu>
2017-05-13 14:11:52 -0700

    recover sim_policy

---
+++ private_examples/sim_policy.py
@@ -0,0 +1,78 @@
+import argparse
+
+import joblib
+import tensorflow as tf
+import numpy as np
+from rllab.misc import tensor_utils
+import time
+from rllab.misc.console import query_yes_no
+'''
+Rollout mlp policy deterministically - using mean.
+'''
+def rollout(env, agent, max_path_length=np.inf, animated=False, speedup=1,
+            always_return_paths=False):
+    observations = []
+    actions = []
+    rewards = []
+    agent_infos = []
+    env_infos = []
+    o = env.reset()
+    agent.reset()
+    path_length = 0
+    if animated:
+        env.render()
+    while path_length < max_path_length:
+        a_sampled, agent_info = agent.get_action(o)
+        # use the mean here for eval.
+        a = agent_info['mean']
+        next_o, r, d, env_info = env.step(a)
+        observations.append(env.observation_space.flatten(o))
+        rewards.append(r)
+        actions.append(env.action_space.flatten(a))
+        agent_infos.append(agent_info)
+        env_infos.append(env_info)
+        path_length += 1
+        if d:
+            break
+        o = next_o
+        if animated:
+            env.render()
+            timestep = 0.05
+            # No slowmotion
+            # time.sleep(timestep / speedup)
+    # if animated and not always_return_paths:
+    #     return
+
+    return dict(
+        observations=tensor_utils.stack_tensor_list(observations),
+        actions=tensor_utils.stack_tensor_list(actions),
+        rewards=tensor_utils.stack_tensor_list(rewards),
+        agent_infos=tensor_utils.stack_tensor_dict_list(agent_infos),
+        env_infos=tensor_utils.stack_tensor_dict_list(env_infos),
+    )
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', type=str,
+                        help='path to the snapshot file')
+    parser.add_argument('--max_path_length', type=int, default=1000,
+                        help='Max length of rollout')
+    parser.add_argument('--speedup', type=float, default=1,
+                        help='Speedup')
+    args = parser.parse_args()
+
+    # If the snapshot file use tensorflow, do:
+    # import tensorflow as tf
+    # with tf.Session():
+    #     [rest of the code]
+    with tf.Session() as sess:
+        data = joblib.load(args.file)
+        policy = data['policy']
+        env = data['env']
+        while True:
+            path = rollout(env, policy, max_path_length=args.max_path_length,
+                           animated=True, speedup=args.speedup)
+            if not query_yes_no('Continue simulation?'):
+                break
+
